aa08f947816e9ec6418f24df3088710fe9693623
Dinkles <isinetov@mail.ru>
2024-07-23 15:38:41 +0400

    Добавил сущности (BookEntity, UserEntity), которые будут храниться в БД.

---
+++ OnlineLibraryEF/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineLibraryEF.Entities
+{
+    internal class UserEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string Role { get; set; }
+    }
+}
