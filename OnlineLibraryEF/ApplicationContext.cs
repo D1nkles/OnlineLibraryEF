49df023b6334d6c9ce41606e6ef2f442759ca2ff
Dinkles <isinetov@mail.ru>
2024-07-23 15:59:43 +0400

    Добавил класс ApplicationContext, который наследуется от системного класса EntityFrameworkCore.DbContext и является контекстом данного приложения, в котором реализуется подключение к БД и объявляются данные, которые будут хранится в таблицах в БД.

---
+++ OnlineLibraryEF/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineLibraryEF.Entities;
+
+namespace OnlineLibraryEF
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<BookEntity> Books { get; set; }
+
+        public ApplicationContext() 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ILIA\SQLEXPRESS;Database=OnlineLibrary_EF;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
