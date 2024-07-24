93772a4c53ffb807ac579ea162401292f67c64d9
Dinkles <isinetov@mail.ru>
2024-07-24 22:43:40 +0400

    Ошибка заключалась в отсутвии модификатора доступа у навигационного свойства в UserEntity. Оказывается, private поля EF не рассматривает при создании таблиц.

--- OnlineLibraryEF/Program.cs
+++ OnlineLibraryEF/Program.cs
@@ -1,10 +1,21 @@
 ﻿using OnlineLibraryEF;
+using OnlineLibraryEF.Entities;
 using OnlineLibraryEF.Repositories;
 
 internal class Program 
 {
     static void Main(string[] args) 
     {
-        using (var db = new ApplicationContext()) { }
+        using (var db = new ApplicationContext()) 
+        {
+            var user = new UserEntity() { Name = "ТутИмя", Email = "sample@gmail.com" };
+            var book = new BookEntity() { Title = "Книга о всяком", ReleaseYear = 1984};
+
+            book.Users.Add(user);
+            
+            db.Users.Add(user);
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
     }
 }
